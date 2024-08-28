63841fdec5eba6742a4780871cfebfe0ea176c3d
Israel Grande <i.grande46@gmail.com>
2024-08-28 15:39:12 +0100

    This commit will add an endpoint that gets all comments by article id, controller and model to handle request, response and query of it, and a TDD to ensure the correct behaviour. It also adds an error handling for invalid id input of an article, and the updated endpoints.json file as well as the refactors requested by PR4

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const { getTopics } = require("./controllers/topics.controllers");
 const { getApi } = require("./controllers/api.controllers");
 const { getArticleById } = require("./controllers/articles.controllers");
 const { getArticles } = require("./controllers/articles.controllers");
+const { getCommentsByArticle } = require("./controllers/articles.controllers");
 const app = express();
-
-app.use(express.json());
 
 app.get("/api", getApi);
 
@@ -14,6 +13,23 @@ app.get("/api/topics", getTopics);
 app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles/:article_id/comments", getCommentsByArticle);
+
+// Refactored Middleware to handle custom errors as requested in (PR#4)
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    return res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    return res.status(400).send({ msg: "Invalid input" });
+  }
+  next(err);
+});
 
 //Refactor of err truthy, as it can handle any error
 app.use((err, req, res, next) => {
